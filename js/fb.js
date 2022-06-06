987bb4c5ffa7855c0422f2f13954aee85fbe47f6
Abdul Basit <abdulbasitansari288@gmail.com>
2022-06-06 23:28:04 +0500

    Initial Commit

--- js/fb.js
+++ js/fb.js
@@ -26,20 +26,80 @@ function c1() {
 
 
 
+
+function create() {
+console.log("clicled")
+first_name = document.getElementById("fname").value
+last_name = document.getElementById("lname").value
+mobile = document.getElementById("mobile").value
+pword = document.getElementById("pword").value
+date = document.getElementById("date").value
+month = document.getElementById("month").value
+y = document.getElementById("year").value
+year = Number(y)
+console.log( year +1)
+if (first_name.length < 3 || last_name.length < 3) {
+    alert("First Name And Last Name Is Must Be Grater Then 3 Characters")
+    
+}
+else if (year > 2007) {
+    alert("Sorry You Can't Signup Because Your Age Is Less Than 15 Years")
+    
+}
+else if (pword.length < 4) {
+    alert("Please Choose 4 Character or 4+ Password")
+    
+}
+else if (first_name === "" || last_name === "" || mobile === ""|| pword === "") {
+    alert("Please Fill All Section")
+    
+}
+
+// else if(first_name.length <! 3 && last_name.length <! 3 && year <! 2007&& pword.length <! 4 && first_name != "" && last_name != "" && mobile != "" && pword != "") {
+
+//     document.getElementById('body').style.opacity = 1;
+//        document.getElementById('form2').style.display = "none";
+
+//        f2();
+// }
+ else{
+    document.getElementById('body').style.opacity = 1;
+    document.getElementById('form2').style.display = "none";
+    alert("YOUR ACCOUNT IS CREATED PLEASE FILL LOGIN FORM & LOGIN")
+    
+    }
+ 
+
+
+
+
+
+}
+
 function f2() {
     // var lname = document.getElementById('lname').value;
     // var username = fname + lname;
-    var fname = document.getElementById('fname').value;
-    var cpword = document.getElementById('cpword').value;
-    var uname = document.getElementById('uname').value;
-    var pword = document.getElementById('pword').value;
-    if (fname === uname && cpword === pword) {
+console.log("Sorry bhai")
+    let mobile = document.getElementById("mobile").value
+    let pword = document.getElementById("pword").value
+    let lmobile = document.getElementById('lfname').value;
+    let lpword = document.getElementById('cpword').value;
+    console.log(mobile , pword , lmobile ,lpword)
+    if (lmobile === mobile && lpword === pword) {
         alert('Log In Success');
+        
+
+    } 
+    if (lmobile === "" || lpword === "") {
+        alert('Please Fill Both Sections');
         return false;
-
-    } else {
+        
+    } 
+    
+    if (lmobile != mobile || lpword != pword) {
         alert('User Id OR Password Is Incorrect');
-
-    }
+        
+        
+    } 
 
 }
